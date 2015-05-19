42c19eebc271b08cc7ecf11bab01ae8136cbfc67
tzvi <tzvigons@google.com>
2015-05-19 14:40:56 -0400

    makeSwatches: add 'forgotten color' prompt

--- Imagick/makeSwatches.bat
+++ Imagick/makeSwatches.bat
@@ -1,6 +1,5 @@
+@SETLOCAL ENABLEDELAYEDEXPANSION
 @ECHO OFF
-SETLOCAL 
-ENABLEDELAYEDEXPANSION
 REM Has this been tested?
 @REM Will create a png file 100x100 pixels with the specified color, named the-specified-color.png
 REM You need to have imconvert in your system path.
@@ -11,7 +10,12 @@ rem @enabledelayedexpansion
 
 
 SET filetype=png
-
+IF [%1]==[] (
+  ECHO No color indicated!
+  ECHO Expected format: makeSwatches color name
+  ECHO Example: makeSwatches dark blue
+  GOTO:EOF
+)
 IF [%2]==[] (
 SET output=%1.%filetype%
 rem echo 2 is not defined
